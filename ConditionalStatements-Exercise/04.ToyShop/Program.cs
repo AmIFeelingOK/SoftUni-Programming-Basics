77e8e5cd5367a18c8a7449e0403cdfc91b443a66
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-08 15:43:58 +0300

    Add files via upload

---
+++ ConditionalStatements-Exercise/04.ToyShop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04.ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tourPrice = double.Parse(Console.ReadLine());
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int teddyBears = int.Parse(Console.ReadLine());
+            int minions = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+
+            int toys = puzzles + dolls + teddyBears + minions + trucks;
+
+            double sum = (puzzles * 2.60)
+                + (dolls * 3)
+                + (teddyBears * 4.10)
+                + (minions * 8.20)
+                + (trucks * 2);
+
+            if (toys >= 50)
+            {
+                sum *= 0.75;
+            }
+
+            double rent = sum * 0.10;
+
+            double finalSum = sum - rent;
+
+            if (finalSum >= tourPrice)
+            {
+                double moneyRemaining = finalSum - tourPrice;
+                Console.WriteLine($"Yes! {moneyRemaining:f2} lv left.");
+            }
+            else
+            {
+                double moneyNeeded = tourPrice - finalSum;
+                Console.WriteLine($"Not enough money! {moneyNeeded:f2} lv needed.");
+            }
+        }
+    }
+}
---
+++ ConditionalStatements-Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double worldRecord = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double timeForOneMeter = double.Parse(Console.ReadLine());
+
+            double addedTime = Math.Floor(meters / 15) * 12.5;
+
+            double totalTime = meters * timeForOneMeter + addedTime;
+
+            if (totalTime < worldRecord)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                double timeRequired = totalTime - worldRecord;
+                Console.WriteLine($"No, he failed! He was {timeRequired:f2} seconds slower.");
+            }
+        }
+    }
+}
