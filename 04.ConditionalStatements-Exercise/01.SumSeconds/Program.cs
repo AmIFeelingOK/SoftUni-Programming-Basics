df196e56b33e22f1b7e2e8bef268f3e9ab4c8d6e
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-10 20:54:54 +0300

    Add files via upload

---
+++ 04.ConditionalStatements-Exercise/01.SumSeconds/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConditionalStatements_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int timeInSeconds = firstTime 
+                + secondTime 
+                + thirdTime;
+
+            int timeInMinutes = timeInSeconds / 60;
+            int secondsRemaining = timeInSeconds % 60;
+
+            if (secondsRemaining < 10)
+            {
+                Console.WriteLine($"{timeInMinutes}:0{secondsRemaining}");
+            }
+            else
+            {
+                Console.WriteLine($"{timeInMinutes}:{secondsRemaining}");
+            }
+        }
+    }
+}
---
+++ 04.ConditionalStatements-Exercise/05.GodzillaVs.Kong/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.GodzillaVs.Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int people = int.Parse(Console.ReadLine());
+            double clothingPriceForOnePerson = double.Parse(Console.ReadLine());
+
+            double decor = budget * 0.10;
+            double clothingPrice = people * clothingPriceForOnePerson;
+            
+            if (people > 150)
+            {
+                clothingPrice *= 0.9;
+            }
+
+            double movieFunds = decor + clothingPrice;
+
+            if (movieFunds <= budget)
+            {
+                double moneyLeft = budget - movieFunds;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {moneyLeft:f2} leva left.");
+            }
+            else
+            {
+                double moneyRequired = movieFunds - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {moneyRequired:f2} leva more.");
+            }
+        }
+    }
+}
