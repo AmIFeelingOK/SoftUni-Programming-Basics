4583e3173ba1f3f349f1f9799b310e5487905c92
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-10 20:53:08 +0300

    Add files via upload

---
+++ 01.FirstStepsInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int otherAnimals = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = dogs * 2.50;
+            double otherAnimalsFoodPrice = otherAnimals * 4;
+            double finalSum = dogFoodPrice + otherAnimalsFoodPrice;
+
+            Console.WriteLine("{0} lv.", finalSum);
+        }
+    }
+}
---
+++ 02.FirstStepsInCoding-Exercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int veganMenus = int.Parse(Console.ReadLine());
+
+            double sum = (chickenMenus * 10.35)
+                + (fishMenus * 12.40)
+                + (veganMenus * 8.15);
+
+            double desert = sum * 0.20;
+
+            double finalSum = sum + desert + 2.5;
+
+            Console.WriteLine(finalSum);
+        }
+    }
+}
