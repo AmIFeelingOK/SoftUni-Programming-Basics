536a6cc862617625305c3a24556d69491fdb652a
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-10 20:46:19 +0300

    Add files via upload

---
+++ ForLoop-Lab/08.NumberSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > max)
+                {
+                    max = number;
+                }
+
+                if (number < min)
+                {
+                    min = number;                    
+                }
+            }
+
+            Console.WriteLine("Max number: {0}\nMin number: {1}", max, min);
+        }
+    }
+}
---
+++ ForLoop-Lab/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < n * 2; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    firstSum += number;
+                }
+                else
+                {
+                    secondSum += number;
+                }
+            }
+
+            if (firstSum == secondSum)
+            {
+                Console.WriteLine($"Yes, sum = {firstSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(firstSum - secondSum)}");
+            }
+        }
+    }
+}
