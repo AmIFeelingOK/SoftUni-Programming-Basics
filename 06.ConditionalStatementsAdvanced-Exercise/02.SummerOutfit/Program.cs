907c1763be25e35b153515b09ef1a73d2e58df50
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-10 20:58:09 +0300

    Add files via upload

---
+++ 06.ConditionalStatementsAdvanced-Exercise/02.SummerOutfit/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _02.SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degreese = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+
+            string outfit = string.Empty;
+            string shoes = string.Empty;
+
+            if (time == "Morning")
+            {
+                if (degreese >= 10 && degreese <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (degreese > 18 &&degreese <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degreese >= 25)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (time == "Afternoon")
+            {
+                if (degreese >= 10 && degreese <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degreese > 18 && degreese <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (degreese >= 25)
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else if (time == "Evening")
+            {
+                if (degreese >= 10 && degreese <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degreese > 18 && degreese <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degreese >= 25)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+
+            Console.WriteLine($"It's {degreese} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
---
+++ 06.ConditionalStatementsAdvanced-Exercise/04.FishingBoat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace fishingBoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermans = int.Parse(Console.ReadLine());
+
+            double shipCost = 0.0;
+
+            if (season == "Spring")
+            {
+                shipCost = 3000;
+            }
+            else if (season == "Summer" || season == "Autumn")
+            {
+                shipCost = 4200;
+            }
+            else if (season == "Winter")
+            {
+                shipCost = 2600;
+            }
+
+            if (fishermans <= 6)
+            {
+
+                shipCost *= 0.9;
+            }
+            else if (fishermans >= 7 && fishermans <= 11)
+            {
+                shipCost *= 0.85;
+            }
+            else if (fishermans >= 12)
+            {
+                shipCost *= 0.75;
+            }
+
+            if (fishermans % 2 == 0 && season != "Autumn")
+            {
+                shipCost *= 0.95;
+            }
+
+            if (shipCost > budget)
+            {
+                double moneyRequired = shipCost - budget;
+                Console.WriteLine($"Not enough money! You need {moneyRequired:f2} leva.");
+            }
+            else if (shipCost <= budget)
+            {
+                double moneyLeft = budget - shipCost;
+                Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+            }
+
+        }
+    }
+}
