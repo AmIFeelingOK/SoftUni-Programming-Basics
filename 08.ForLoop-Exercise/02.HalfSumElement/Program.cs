9bba46988540ab543b08c4274b884ac33803fdf1
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-11 17:14:54 +0300

    Add files via upload

---
+++ 08.ForLoop-Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int max = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                sum += number;
+
+                if (max < number)
+                {
+                    max = number;
+                }
+            }
+
+            int maxFreeSum = sum - max;
+
+            if (max == maxFreeSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", max);
+            }
+            else
+            {
+                int difference = Math.Abs(max - maxFreeSum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", difference);
+            }
+        }
+    }
+}
---
+++ 08.ForLoop-Exercise/03.Histogram/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _03.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double firstCount = 0;
+            double secondCount = 0;
+            double thirdCount = 0;
+            double forthCount = 0;
+            double fifthCount = 0;
+
+            double p1 = 1;
+            double p2 = 1;
+            double p3 = 1;
+            double p4 = 1;
+            double p5 = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                if (numbers < 200)
+                {
+                    firstCount++;
+                }
+                else if (numbers >= 200 && numbers <= 399)
+                {
+                    secondCount++;
+                }
+                else if (numbers >= 400 && numbers <= 599)
+                {
+                    thirdCount++;
+                }
+                else if (numbers >= 600 && numbers <= 799)
+                {
+                    forthCount++;
+                }
+                else if (numbers >= 800)
+                {
+                    fifthCount++;
+                }
+            }
+
+            p1 = firstCount / n * 100;
+            p2 = secondCount / n * 100;
+            p3 = thirdCount / n * 100;
+            p4 = forthCount / n * 100;
+            p5 = fifthCount / n * 100;
+
+            Console.WriteLine("{0:f2}%\n{1:f2}%\n{2:f2}%\n{3:f2}%\n{4:f2}%", p1, p2, p3, p4, p5);
+        }
+    }
+}
