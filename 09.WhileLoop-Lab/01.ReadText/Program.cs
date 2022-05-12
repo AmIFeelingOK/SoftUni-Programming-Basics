e5eceecbe2314604078cede020f5dca4f4b27076
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-12 23:56:19 +0300

    Add files via upload

---
+++ 09.WhileLoop-Lab/01.ReadText/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09.WhileLoop_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                Console.WriteLine(input);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
