aad23c243eb9d94dd8df4f0628f17cb7c5a5d3fa
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-12 23:22:45 +0300

    Add files via upload

---
+++ 08.ForLoop-Exercise/05.Salary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tabs; i++)
+            {
+                string sites = Console.ReadLine();
+
+                if (sites == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (sites == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (sites == "Reddit")
+                {
+                    salary -= 50;
+                }
+
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
