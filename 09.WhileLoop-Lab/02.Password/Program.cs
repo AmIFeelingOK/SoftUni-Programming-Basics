d58aff83df26c80bbd69cc979253b2f9a8568de4
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-13 00:37:08 +0300

    Add files via upload

---
+++ 09.WhileLoop-Lab/02.Password/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02.Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+
+            while (password != "pass")
+            {
+                password = Console.ReadLine();
+            }
+        }
+    }
+}
