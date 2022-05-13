c912f37b7034039d1510b1c1cffe089148514d9b
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-13 19:17:28 +0300

    Add files via upload

---
+++ 09.WhileLoop-Lab/05.AccountBalance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double sum = 0.0;
+
+            while (input != "NoMoreMoney")
+            {
+                double money = double.Parse(input);
+
+                if (money >= 0)
+                {
+                    sum += money;
+                    Console.WriteLine("Increase: {0:f2}", money);
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
