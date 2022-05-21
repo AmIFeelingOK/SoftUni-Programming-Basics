348c92ef4d43d8506dbc4e6a0f0d350945e8dac2
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-21 16:33:12 +0300

    Add files via upload

---
+++ 10.NestedLoops-Lab/05.Travelling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while (destination != "End")
+            {                
+                double budget = double.Parse(Console.ReadLine());
+
+                double sum = 0;
+
+                while (sum < budget)
+                {
+                    double savedMoney = double.Parse(Console.ReadLine());
+                    sum += savedMoney;
+                }
+
+                if (sum >= budget)
+                {
+                    Console.WriteLine("Going to {0}!", destination);
+                }
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
