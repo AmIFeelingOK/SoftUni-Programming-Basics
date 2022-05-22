65aadd94b9fb667673ed886e06ece513d82e94d5
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-05-22 23:42:00 +0300

    Update Program.cs

--- 10.WhileLoop-Exercise/04.Walking/Program.cs
+++ 10.WhileLoop-Exercise/04.Walking/Program.cs
@@ -6,7 +6,24 @@ namespace _04.Walking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totalSteps = 0;
+
+            while (totalSteps < 10000)
+            {
+                string stepsPerGoingOut = Console.ReadLine();
+                if (stepsPerGoingOut == "Going home")
+                {
+                    totalSteps += int.Parse(Console.ReadLine());
+                    break;
+                }
+                int steps = int.Parse(stepsPerGoingOut);
+                totalSteps += steps;
+            }
+
+            if (totalSteps < 10000)
+                Console.WriteLine($"{10000 - totalSteps} more steps to reach goal.");
+            else
+                Console.WriteLine($"Goal reached! Good job!\n{totalSteps - 10000} steps over the goal!");
         }
     }
 }
